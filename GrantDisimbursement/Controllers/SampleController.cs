fe02a5c929ece99e11991c425e0884ab2b067a45
Nicholas Ng Nuo Yang <nicholas.ng@anacle.com>
2020-11-23 01:02:56 +0800

    Initialize skeleton, test connection to MSSQL Server

---
+++ GrantDisimbursement/Controllers/SampleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GrantDisimbursement.Controllers
+{
+    /// <summary>
+    /// To Test Connection To DB
+    /// </summary>
+    public class SampleController : ApiController
+    {
+        [HttpGet]
+        [Route("api/testConnection")]
+        public HttpResponseMessage TestConnection()
+        {
+            SqlConnection cnn;
+            cnn = new SqlConnection(ConfigurationManager.AppSettings["database"]);
+            cnn.Open();
+            cnn.Close();
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ GrantDisimbursement/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+
+namespace GrantDisimbursement
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
