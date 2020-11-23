ceb7de7e3edc7bce5dd16e552088c99baf24e6eb
nicholas.n3y <nicholas.n3y@gmail.com>
2020-11-24 00:56:05 +0800

    Add API to retrieve single household

--- GrantDisimbursement/Controllers/HouseholdController.cs
+++ GrantDisimbursement/Controllers/HouseholdController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Configuration;
+using System.Data;
 
 namespace GrantDisimbursement.Controllers
 {
@@ -70,5 +71,104 @@ namespace GrantDisimbursement.Controllers
             }
         }
 
+        /// <summary>
+        /// Retrieves a specific  household
+        /// </summary>
+        /// <param name="householdID">the identifier of the household</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{householdID}")]
+        public IHttpActionResult RetrieveHousehold(Guid? householdID)
+        {
+            if (householdID == null)
+                return BadRequest("householdID must be specified");
+
+            // Return HouseholdListingResponse
+            HouseholdEntityResponse householdEntityResponse = new HouseholdEntityResponse();
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+
+            string sql = @"SELECT ObjectID, HousingType, IsDeleted FROM Household WHERE IsDeleted = 0 and ObjectID = @ObjectID";
+
+            using (SqlConnection c = new SqlConnection(ConfigurationManager.AppSettings["database"]))
+            {
+                command = new SqlCommand(sql, c);
+                command.Parameters.Add(new SqlParameter("@ObjectID", householdID));
+                c.Open();
+                dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    householdEntityResponse.Household = ReadHouseholdRow((IDataRecord)dataReader);
+                }
+
+                if (householdEntityResponse.Household == null)
+                    return Ok("Household does not exist");
+            }
+
+            using (SqlConnection c = new SqlConnection(ConfigurationManager.AppSettings["database"]))
+            { 
+                sql = @"SELECT [ObjectID],
+                    [Name],
+                    [Gender],
+                    [MaritalStatus],
+                    [SpouseID],
+                    [OccupationType],
+                    [AnnualIncome],
+                    [DateOfBirth],
+                    [HouseholdID],
+                    [IsDeleted] FROM [Member] 
+                    WHERE [IsDeleted] = 0 AND HouseholdID = @HouseholdID";
+
+                c.Open();
+                command = new SqlCommand(sql, c);
+                command.Parameters.Add(new SqlParameter("@HouseholdID", householdID));
+
+                dataReader = command.ExecuteReader();
+                householdEntityResponse.FamilyMembers = new List<Member>();
+                while (dataReader.Read())
+                {
+                    householdEntityResponse.FamilyMembers.Add(ReadMemberRow((IDataRecord)dataReader));
+                }
+            }
+            return Ok(householdEntityResponse);
+        }
+
+        private Household ReadHouseholdRow(IDataRecord record)
+        {
+            return new Household()
+            {
+                ObjectID = (Guid?)record[0],
+                HousingType = (string)record[1],
+                IsDeleted = (int?)record[2]
+            };
+        }
+
+        private Member ReadMemberRow(IDataRecord record)
+        {
+            Member m = new Member();
+            m.ObjectID = record.GetGuid(0);
+            m.Name = record.GetString(1);
+            m.Gender = record.GetString(2);
+            m.MaritalStatus = record.GetString(3);
+
+            if (record.IsDBNull(4))
+                m.SpouseID = null;
+            else
+                m.SpouseID = record.GetGuid(4);
+
+            m.OccupationType = record.GetString(5);
+            m.AnnualIncome = record.GetDecimal(6);
+            m.DateOfBirth = record.GetDateTime(7);
+
+            if (record.IsDBNull(8))
+                m.HouseholdID = null;
+            else
+                m.HouseholdID = record.GetGuid(8);
+
+            m.IsDeleted = record.GetInt32(9);
+            return m;
+        }
     }
 }
---
+++ GrantDisimbursement/Models/HouseholdEntityResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrantDisimbursement.Models
+{
+    public class HouseholdEntityResponse
+    {
+        public Household Household { get; set; }
+        public List<Member> FamilyMembers { get; set; }
+    }
+}
